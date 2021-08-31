21dc19f4e2a6822e440cf7a7afce2f1b8cd285ce
Saif Ullah Khalid <saifkhalid467@gmail.com>
2021-08-30 23:18:33 -0700

    updated

--- external.js
+++ external.js
@@ -33,9 +33,11 @@ function getNameB(player) {
     }
 
 }
+
 let win;
+let x;
 function toss() {
-    let x;
+    // let x;
     let rand = Math.floor(Math.random() * 2) + 0;
     if (rand == 1) {
         let x = prompt("Team A won the toss and choose to:");
@@ -55,125 +57,179 @@ function toss() {
 
     }
     display(win);
-   
+
 }
+const innings = [
+    {
+        teamName: "",
+        totalRuns: 0,
+        ball: 0,
+        overs: 0,
+        wickets: 0,
+        choice: "",
+        // scoreSeet: [
+        //     {
+        //         runs: 0,
+        //         isWicket: "",
+        //         isNoball: "",
+        //         isWideBall: ""
+        //     }
+        //     // {
+        //     //     runs: 3,
+        //     //     ballCount: 2,
+        //     //     isWicket: false,
+        //     //     isNoball: false,
+        //     //     isWideBall: false
+        //     // }
+        // ]
+    },
+    {
+        teamName: "",
+        totalRuns: 0,
+        ball: 0,
+        overs: 0,
+        wickets: 0,
+        choice: "",
+        // scoreSeet: [
+        //     {
+        //         runs: 0,
+        //         ballCount: 1,
+        //         isWicket: false,
+        //         isNoball: false,
+        //         isWideBall: false
+        //     }
+        //     // {
+        //     //     runs: 3,
+        //     //     ballCount: 2,
+        //     //     isWicket: false,
+        //     //     isNoball: false,
+        //     //     isWideBall: false
+        //     // }
+        // ]
+    }
+]
 
-
-
-let totalScore = []; 
-let ball = 0;
-let overs = 0;
+let totalScore = [];
+let overs ;
 
 function result() {
-    let wt = 0;
-    let wd = 0;
-    let nb = 0;
-    let totalRuns = 0;
+    let wd ;
+    let nb ;
     let runs = Math.floor(Math.random() * 6) + 1;
+    
+    innings[0].ball +=1
+    innings[0].overs += 0.1
+    num = innings[0].overs.toFixed(1);
+    
 
-    ball+=1
-    overs += 0.1;
-    num  = overs.toFixed(1);
-
-    if (ball == 6) {
+    if (innings[0].ball == 6) {
         num = "1"
-        
-    }else if(ball == 7){
+
+    } else if (innings[0].ball == 7) {
         num = "1.1"
-        wt+=1
-        totalRuns+=-runs;
+        innings[0].wickets += 1
+        innings[0].totalRuns += -runs;
 
-    }else if(ball == 8){
+    } else if (innings[0].ball == 8) {
         num = "1.1"
-         wt+=1
-         wd+=1
-      
-    }else if(ball == 9){
-        num = "1.2"
-        wt+=1
+        wd += 1
 
-    }else if(ball == 10){
+    } else if (innings[0].ball == 9) {
         num = "1.2"
-        wt+=1
-        nb+=1
+    
+    } else if (innings[0].ball == 10) {
+        num = "1.2"
+        nb += 1
 
-    }else if(ball == 11){
+    } else if (innings[0].ball == 11) {
         num = "1.3"
-        wt+=1
-
-    }else if(ball == 12){
+    
+    } else if (innings[0].ball == 12) {
         num = "1.4"
-        wt+=1
-
-    }else if(ball == 13){
+  
+    } else if (innings[0].ball == 13) {
         num = "1.5"
-        wt+=2
-        totalRuns+=-runs;
+        innings[0].wickets += 1
+        innings[0].totalRuns += -runs;
 
-    }else if(ball == 14){
+    } else if (innings[0].ball == 14) {
         num = "2"
-        wt+=2
-      
+    }
+    if(innings[0].ball == 14){
+        document.getElementById("ball").disabled = true;
     }
 
     total = ({
         runs,
-        ball,
         overs
-        
+
     })
-
-        totalScore = [...totalScore, total];
-    
-        totalScore.forEach(element => {
-         totalRuns+=element.runs;
-
-        });
-
-    win = win.split(" ")[0]
-    win = `${win}  ${totalRuns} - ${wt}  Overs: ${num} `
-    display(win);
    
-    if (ball==14) {
-        document.getElementById("ball").disabled = true;
-    }
-    if (ball==7) {
+    totalScore = [...totalScore, total];
+    
+    innings[0].totalRuns += runs;
+
+
+    // totalScore.forEach(element => {
+    //     innings[0].totalRuns += element.runs;
+    //     // console.log(innings[0].totalRuns);
+    //     // console.log(element.runs);
+
+    // });
+
+//    console.log(innings);
+   
+    win = win.split(" ")[0]
+    win = `${win}  ${innings[0].totalRuns} - ${innings[0].wickets}  Overs: ${num} `
+    display(win);
+
+
+    if (innings[0].ball == 7) {
         document.getElementById("wicket").style.display = "block";
     }
-    else{
-        setInterval(clear, 4000);
+    else {
+        setInterval(clear, 3000);
         function clear() {
             let targetOne = document.getElementById("wicket");
             targetOne.style.display = "none";
         }
     }
-    if (ball==8) {
+    if (innings[0].ball == 13) {
+        document.getElementById("wicket").style.display = "block";
+    }
+    else {
+        setInterval(clear, 3000);
+        function clear() {
+            let targetOne = document.getElementById("wicket");
+            targetOne.style.display = "none";
+        }
+    }
+    if (innings[0].ball == 8) {
         document.getElementById("wide").style.display = "block";
     }
-    else{
-        setInterval(clear, 4000);
+    else {
+        setInterval(clear, 6000);
         function clear() {
             let targetTwo = document.getElementById("wide");
             targetTwo.style.display = "none";
         }
     }
-    if (ball==10) {
+    if (innings[0].ball == 10) {
         document.getElementById("noball").style.display = "block";
     }
-    else{
-        setInterval(clear, 4000);
+    else {
+        setInterval(clear, 7000);
         function clear() {
             let targetThree = document.getElementById("noball");
             targetThree.style.display = "none";
         }
     }
-
 }
 
 
 
 let a = document.createElement("h4");
-const display = (value) =>{
+const display = (value) => {
     let b = document.createTextNode(value);
     while (a.firstChild) {
         a.removeChild(a.firstChild);
@@ -183,3 +239,4 @@ const display = (value) =>{
 
     document.getElementById("toss").style.display = "none";
 }
+
